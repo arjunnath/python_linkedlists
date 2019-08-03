3c8b4341c40968a129ffd0a9b78078b518a563ef
Arjun Nath <arjunnath@gmail.com>
2019-08-03 03:58:11 -0700

    1st cuts

---
+++ linkedlist1.py
@@ -0,0 +1,7 @@
+
+
+class Node :
+
+	def __init__(self, val):
+		self.value = val
+		self.nextnode = None
---
+++ linkedlist2.py
@@ -0,0 +1,22 @@
+
+
+class Node :
+
+	def __init__(self, val):
+		self.value = val
+		self.nextnode = None
+		self.prevnode = None
+
+	def next(self) :
+		return self.nextnode
+
+	def prev(self):
+		return self.prevnode
+
+	def new(self, val) :
+		i = Node(val)
+		self.nextnode = i
+		i.prevnode = self
+		return i
+
+
