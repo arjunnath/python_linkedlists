7b11241546cfe6e5befe4546096e567cb34edc9e
Arjun R Nath <arjunnath@gmail.com>
2019-08-29 02:07:54 -0700

    Update linkedlist2.py

--- linkedlist2.py
+++ linkedlist2.py
@@ -6,14 +6,42 @@ class Node :
         self.nextnode = None
         self.prevnode = None
 
+
+class DoubleLinkedList :
+    
+    def __init__(self, val=None):
+        self.listptr = None
+        if val is None :
+            return None
+        self.listptr = Node(val)
+        self.__head__ = self.listptr
+
+    def new(self, val=None) :
+        if self.listptr is None :
+            raise Exception("List object has no Node")
+        if val is None :
+            pass # do nothing
+        i = Node(val)
+        if self.listptr.nextnode is None:
+            self.listptr.nextnode = i
+            i.prevnode = self.listptr
+            self.listptr = i
+        else :
+            x = self.listptr.nextnode
+            x.prevnode = i
+            i.prevnode = self.listptr
+            i.nextnode = x
+            self.listptr.nextnode = i
+            self.listptr = i
+
     def next(self) :
-        return self.nextnode
+        if self.listptr.nextnode is not None :
+            self.listptr = self.listptr.nextnode
 
     def prev(self):
-        return self.prevnode
+        if self.listptr.prevnode is not None :
+            self.listptr = self.listptr.prevnode
 
-    def new(self, val) :
-        i = Node(val)
-        self.nextnode = i
-        i.prevnode = self
-        return i
+    def value(self):
+        return self.listptr.value
+
